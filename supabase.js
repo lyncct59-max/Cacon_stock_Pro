d2f79ca2755788b4b20dc0ace644e8aa3e3e4e85
lyncct59-max <lyncct59@gmail.com>
2026-03-31 08:42:21 +0700

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
 window.SUPABASE_URL = "https://unlrlmxlngxhstbnrzax.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_DB9DIVS7RWnE4rEfY4GaHg_Kwc8rhya";
 window.supabaseClient = window.supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
-window.SUPABASE_BUCKET = "charts";
+window.SUPABASE_BUCKET = "cacon-files";

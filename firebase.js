77201068d1323f4551c7dba8fd687c5301a654cd
lyncct59-max <lyncct59@gmail.com>
2026-04-02 14:06:10 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCB8VyWZSc_T89l8s1w_vgzI-se6oz4ql4",
+  authDomain: "cacon-trading-journal.firebaseapp.com",
+  projectId: "cacon-trading-journal",
+  storageBucket: "cacon-trading-journal.firebasestorage.app",
+  messagingSenderId: "720236845512",
+  appId: "1:720236845512:web:cf5b6064ad7f3a4bc988fd",
+  measurementId: "G-2BZJG9NQ71"
+};
+
+firebase.initializeApp(firebaseConfig);
+window.auth = firebase.auth();
+window.db = firebase.firestore();
+window.storage = firebase.storage();

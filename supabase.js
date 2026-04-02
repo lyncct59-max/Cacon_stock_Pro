be63f2dadc4bcb816cf64574ff76fe6f6988176e
lyncct59-max <lyncct59@gmail.com>
2026-04-02 14:03:53 +0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-window.SUPABASE_URL = "https://unlrlmxlngxhstbnrzax.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_DB9DIVS7RWnE4rEfY4GaHg_Kwc8rhya";
-window.supabaseClient = window.supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY);
-window.SUPABASE_BUCKET = "charts";
